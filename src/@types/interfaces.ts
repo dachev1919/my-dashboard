25518adbd64d6795d1b1f47ae8d4101664815845
Dachev <dachev1919@gmail.com>
2023-03-08 22:41:45 +0200

    project init

---
+++ src/@types/interfaces.ts
@@ -0,0 +1,71 @@
+export interface ITeam {
+  id: number;
+  name: string;
+  email: string;
+  age: number;
+  phone: string;
+  access: string;
+}
+
+export interface IContact {
+  id: number;
+  name: string;
+  email: string;
+  age: number;
+  phone: string;
+  address: string;
+  city: string;
+  zipCode: string;
+  registrarId: number;
+}
+
+export interface IInvoice {
+  id: number;
+  name: string;
+  email: string;
+  cost: string;
+  phone: string;
+  date: string;
+}
+
+export interface ITransaction {
+  txId: string;
+  user: string;
+  date: string;
+  cost: string;
+}
+
+export interface IBar {
+  country: string;
+  "hot dog": number;
+  "hot dogColor": string;
+  burger: number;
+  burgerColor: string;
+  kebab: number;
+  kebabColor: string;
+  donut: number;
+  donutColor: string;
+}
+
+export interface IPie {
+  id: string;
+  label: string;
+  value: number;
+  color: string;
+}
+
+export interface ILine {
+  id: string;
+  color: string;
+  data: ILineData[];
+}
+
+interface ILineData {
+  x: string;
+  y: number;
+}
+
+export interface IGeography {
+  id: string;
+  value: number;
+}
---
+++ src/modules/App.tsx
@@ -0,0 +1,15 @@
+import { FunctionComponent } from 'react';
+import Layout from '../common/components/layout/Layout';
+import { Routers } from '../routes/Routers';
+
+interface IAppProps {}
+
+const App: FunctionComponent<IAppProps> = () => {
+	return (
+    <Layout>
+      <Routers />
+    </Layout>
+	);
+};
+
+export default App;
