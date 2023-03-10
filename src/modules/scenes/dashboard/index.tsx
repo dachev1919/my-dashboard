5167b128e219f706c26da3f32cb7fd6f556b3141
Dachev <dachev1919@gmail.com>
2023-03-10 11:25:01 +0200

    added dashboard title

--- src/modules/scenes/dashboard/index.tsx
+++ src/modules/scenes/dashboard/index.tsx
@@ -6,6 +6,7 @@ const Dashboard: FC = () => {
 	return (
 		<Box m='20px'>
 			<Box display='flex' justifyContent='space-between' alignItems='center'>
+
 				<Header title='DASHBOARD' subtitle='Welcome to your dashboard' />
 			</Box>
 		</Box>
