36f0e53229d0cc163205bb316bce2d6ce621475d
Dachev <dachev1919@gmail.com>
2023-03-10 11:24:22 +0200

    added header

--- src/modules/scenes/dashboard/index.tsx
+++ src/modules/scenes/dashboard/index.tsx
@@ -1,10 +1,15 @@
 import { FC } from 'react';
+import Header from '../../../common/components/header/Header';
+import { Box } from '@mui/material';
 
 const Dashboard: FC = () => {
-
-  return (
-    <h1></h1>
-  );
+	return (
+		<Box m='20px'>
+			<Box display='flex' justifyContent='space-between' alignItems='center'>
+				<Header title='DASHBOARD' subtitle='Welcome to your dashboard' />
+			</Box>
+		</Box>
+	);
 };
 
 export default Dashboard;
