b8c91d88bf46fb5f4cffd0b20de78bdbfa11f52d
Dachev <dachev1919@gmail.com>
2023-03-14 17:58:49 +0200

    added form page

--- src/routes/Routers.tsx
+++ src/routes/Routers.tsx
@@ -4,6 +4,7 @@ import Dashboard from "../modules/scenes/dashboard";
 import Team from "../modules/scenes/team";
 import Contacts from "../modules/scenes/contacts";
 import Invoices from "../modules/scenes/invoices";
+import Form from "../modules/scenes/form";
 
 interface RoutersProps {}
 
@@ -17,7 +18,7 @@ export const Routers: FC<RoutersProps> = () => {
         <Route path='/my-dashboard/contacts' element={<Contacts />} />
         <Route path='/my-dashboard/invoices' element={<Invoices />} />
         {/*<Route path='/my-dashboard/bar' element={<Bar />} />*/}
-        {/*<Route path='/my-dashboard/form' element={<Form />} />*/}
+        <Route path='/my-dashboard/form' element={<Form />} />
         {/*<Route path='/my-dashboard/line' element={<Line />} />*/}
         {/*<Route path='/my-dashboard/pie' element={<Pie />} />*/}
         {/*<Route path='/my-dashboard/faq' element={<FAQ />} />*/}
