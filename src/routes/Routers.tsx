83d458f8a4e691be388a4da821ef6c566a91cfcb
Dachev <dachev1919@gmail.com>
2023-03-14 12:57:12 +0200

    added contacts page

--- src/routes/Routers.tsx
+++ src/routes/Routers.tsx
@@ -2,15 +2,7 @@ import React, { FC } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from "../modules/scenes/dashboard";
 import Team from "../modules/scenes/team";
-// import Invoices from "../modules/scenes/invoices";
-// import Contacts from "../modules/scenes/contacts";
-// import Bar from "../modules/scenes/bar";
-// import Form from "../modules/scenes/form";
-// import Line from "../modules/scenes/line";
-// import Pie from "../modules/scenes/pie";
-// import FAQ from "../modules/scenes/faq";
-// import Geography from "../modules/scenes/geography";
-// import Calendar from "../modules/scenes/calendar";
+import Contacts from "../modules/scenes/contacts";
 
 interface RoutersProps {}
 
@@ -21,8 +13,8 @@ export const Routers: FC<RoutersProps> = () => {
         <Route path='/' element={<Navigate to='/my-dashboard' />} />
         <Route path='/my-dashboard' element={<Dashboard />} />
         <Route path='/my-dashboard/team' element={<Team />} />
+        <Route path='/my-dashboard/contacts' element={<Contacts />} />
         {/*<Route path='/my-dashboard/invoices' element={<Invoices />} />*/}
-        {/*<Route path='/my-dashboard/contacts' element={<Contacts />} />*/}
         {/*<Route path='/my-dashboard/bar' element={<Bar />} />*/}
         {/*<Route path='/my-dashboard/form' element={<Form />} />*/}
         {/*<Route path='/my-dashboard/line' element={<Line />} />*/}
